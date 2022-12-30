7aad2782e226257d1ec64dfdbea9a5e127ce60d7
MarianoElias03 <mariolovo76@hotmail.com>
2022-12-30 16:13:40 +1000

    update

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,6 @@
 import styles from '../../styles/Slug.module.css'
 import { GraphQLClient, gql } from 'graphql-request'
+import Head from 'next/head';
 
 const graphcmds = new GraphQLClient("https://api-ap-southeast-2.hygraph.com/v2/clc9rtx4y225601t8acvshp51/master")
 
@@ -57,17 +58,26 @@ export async function getStaticProps({params}){
 }
 export default function BlogPost({post}){
     return (
-    <main className={styles.blog}>
-        <img src={post.coverPhoto.url} className={styles.cover} alt=''/>
-        <div className={styles.title}>
-            <img src={post.author.avatar.url} alt=''/>
-            <div className={styles.authtext}>
-                <h6>By {post.author.name}</h6>
-                <h6 className={styles.date}>{post.datePublished}</h6>
+    <>
+        <Head>
+        <title>{post.title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/wriality.png" />
+        <link href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap" rel="stylesheet"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossOrigin="anonymous"/>
+      </Head>
+        <main className={styles.blog}>
+            <img src={post.coverPhoto.url} className={styles.cover} alt=''/>
+            <div className={styles.title}>
+                <img src={post.author.avatar.url} alt=''/>
+                <div className={styles.authtext}>
+                    <h6>By {post.author.name}</h6>
+                    <h6 className={styles.date}>{post.datePublished}</h6>
+                </div>
             </div>
-        </div>
-        <h2>{post.title}</h2>
-        <div className={styles.content} dangerouslySetInnerHTML={{ __html: post.content.html }}></div>
-    </main>
+            <h2>{post.title}</h2>
+            <div className={styles.content} dangerouslySetInnerHTML={{ __html: post.content.html }}></div>
+        </main>
+    </>
     )
 }
