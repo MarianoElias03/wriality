7c25716887ec97ee4ee46506a45548ee7b2b013c
MarianoElias03 <mariolovo76@hotmail.com>
2023-01-01 16:20:38 +1000

    update

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link'
+
+import { getCategories } from '../services';
 
 const Categories = () => {
+  const [categories, setCategories] = useState([]);
+  
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories))
+  }, []);
+
   return (
-    <div>Categories</div>
+    <div className='card mb-3'>
+      <h3 className=' card-body card-title'>
+        Categories
+      </h3>
+      {categories.map((category) => (
+        <Link href={`/category/${category.slug}`} key={category.slug}>
+          
+        </Link>
+      ))}
+    </div>
   );
 };
 
