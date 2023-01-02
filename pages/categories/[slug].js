17bafc2b1f3333bc8de7d0565c13ce86fc085a80
MarianoElias03 <mariolovo76@hotmail.com>
2023-01-02 20:12:20 +1000

    light-theme update

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -2,7 +2,6 @@ import styles from '../../styles/SlugPost.module.css'
 import { GraphQLClient, gql } from 'graphql-request'
 import Head from 'next/head';
 import moment from "moment";
-import Categories from "../../components"
 
 const graphcmds = new GraphQLClient("https://api-ap-southeast-2.hygraph.com/v2/clc9rtx4y225601t8acvshp51/master")
 
